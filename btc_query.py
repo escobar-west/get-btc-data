c9dd619781e5ca7334064df99a9a7532758441f8
Victor Suriel <victor.v.suriel@gmail.com>
2017-08-06 12:22:55 -0400

    Update btc_query.py

--- btc_query.py
+++ btc_query.py
@@ -1,32 +1,44 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug  4 16:39:32 2017
+Created on Sat Aug  5 18:19:06 2017
 
-@author: victor.suriel
+@author: EscobarWest
 """
+
 import time
 import datetime
 import sqlite3
 import requests
+from requests.exceptions import ConnectionError
 
 
-conn = sqlite3.connect('example.db', detect_types=sqlite3.PARSE_DECLTYPES)
+conn = sqlite3.connect('cryptocurrencies.db', detect_types=sqlite3.PARSE_DECLTYPES)
 c = conn.cursor()
 
-if True: # Don't run this statement unless you want to delete the table    
-    c.execute('DROP TABLE btc')
-    c.execute('CREATE TABLE btc (date timestamp, price real)')
+if False: # Don't run this statement unless you want to delete the table    
+    c.execute('DROP TABLE XXBTZUSD')
+    c.execute('''CREATE TABLE XXBTZUSD
+                  (date INT PRIMARY KEY,
+                  price REAL,
+                  volume REAL)''')
 
-url = 'https://www.bitstamp.net/api/ticker/'
+url = 'https://api.kraken.com/0/public/OHLC?pair=XXBTZUSD'
 
-for _ in range(5):
-    raw_data = requests.get(url)
-    price = raw_data.json()['last']
-    
-    c.execute('INSERT INTO btc VALUES (?,?)', (datetime.datetime.today(), price))
-    print('inserted values into table')
-    conn.commit()
-    time.sleep(3)
-    
-for row in c.execute('SELECT * FROM btc'):
-    print(row)
+while(1):
+    try:
+        raw_data = requests.get(url)
+        
+        result = raw_data.json()['result']['XXBTZUSD'][:-1]
+        result_iter = ((row[0],row[4],row[6]) for row in result)
+        
+        c.executemany('INSERT OR IGNORE INTO XXBTZUSD VALUES (?,?,?)', result_iter)
+        conn.commit()
+        
+        print('{}: committed new entries. Now sleeping...'
+              .format(datetime.datetime.fromtimestamp(time.time())))
+        time.sleep(3600)
+    except ConnectionError:
+        print('{}: Could not connect to internet. Retrying in 1 minute...'
+              .format(datetime.datetime.fromtimestamp(time.time())))
+        time.sleep(60)
